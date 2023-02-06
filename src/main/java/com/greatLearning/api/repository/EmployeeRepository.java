f2e98033adef6faaff1734c582a4174b1a49fec3
Saksham <saksham.gairola28@gmail.com>
2023-02-06 22:40:07 +0530

    Added Employee Repository interface

---
+++ src/main/java/com/greatLearning/api/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.greatLearning.api.repository;
+
+import com.greatLearning.api.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
