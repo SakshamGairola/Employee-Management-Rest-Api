de0274f65cd85a090a20d9a322eab6256a1e4fbd
Saksham <saksham.gairola28@gmail.com>
2023-02-06 21:00:38 +0530

    Initial Commit

---
+++ src/main/java/com/greatLearning/api/EmployeeManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.greatLearning.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementRestApiApplication.class, args);
+	}
+
+}
