027b7d01aded8224de8a4709f11622abcf1ca027
Saksham <saksham.gairola28@gmail.com>
2023-02-07 16:32:11 +0530

    Added Search Employee Service implementation class

---
+++ src/main/java/com/greatLearning/api/serviceImpl/EmployeeServiceImpl/SearchEmployeeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.greatLearning.api.serviceImpl.EmployeeServiceImpl;
+
+import com.greatLearning.api.service.EmployeeService.SearchEmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchEmployeeServiceImpl implements SearchEmployeeService {
+}
