6d2e6d4a9b1d298f323169704b1112b9585229b8
Saksham <saksham.gairola28@gmail.com>
2023-02-07 16:32:23 +0530

    Added Sort Employee Service implementation class

---
+++ src/main/java/com/greatLearning/api/serviceImpl/EmployeeServiceImpl/SortEmployeeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.greatLearning.api.serviceImpl.EmployeeServiceImpl;
+
+import com.greatLearning.api.service.EmployeeService.SortEmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SortEmployeeServiceImpl implements SortEmployeeService {
+}
