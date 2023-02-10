6270ee0dbc9270f4fdd194039a10583cb57e175e
Saksham <saksham.gairola28@gmail.com>
2023-02-10 20:19:44 +0530

    added query to get user by name

--- src/main/java/com/greatLearning/api/repository/UserRepository.java
+++ src/main/java/com/greatLearning/api/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.greatLearning.api.repository;
 
 import com.greatLearning.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.userName = ?1")
+    User getUserByName(String userName);
 }
