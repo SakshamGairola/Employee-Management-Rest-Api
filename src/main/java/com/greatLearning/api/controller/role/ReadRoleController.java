7e557e2071016adcfdf2a2601217cade361b639f
Saksham <saksham.gairola28@gmail.com>
2023-02-10 20:14:14 +0530

    Added controller for fetching all 'ROLES'

---
+++ src/main/java/com/greatLearning/api/controller/role/ReadRoleController.java
@@ -0,0 +1,25 @@
+package com.greatLearning.api.controller.role;
+
+import com.greatLearning.api.entity.Role;
+import com.greatLearning.api.serviceImpl.RoleServiceImpl.ReadRoleServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/role")
+public class ReadRoleController {
+
+    @Autowired
+    ReadRoleServiceImpl readService;
+
+
+    @GetMapping("/all")
+    public List<Role> getRoles(){
+        return this.readService.getALL();
+    }
+
+}
