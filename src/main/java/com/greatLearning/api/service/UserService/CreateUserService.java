814a622da85c67f5b0f7e07fa00ebf7420c33baa
Saksham <saksham.gairola28@gmail.com>
2023-02-10 20:21:51 +0530

    added method to persist user

--- src/main/java/com/greatLearning/api/service/UserService/CreateUserService.java
+++ src/main/java/com/greatLearning/api/service/UserService/CreateUserService.java
@@ -1,5 +1,8 @@
 package com.greatLearning.api.service.UserService;
 
+import com.greatLearning.api.entity.User;
+
 public interface CreateUserService {
+    void createUser(User user);
 }
 
--- src/main/java/com/greatLearning/api/serviceImpl/UserServiceImpl/CreateUserServiceImpl.java
+++ src/main/java/com/greatLearning/api/serviceImpl/UserServiceImpl/CreateUserServiceImpl.java
@@ -1,8 +1,19 @@
 package com.greatLearning.api.serviceImpl.UserServiceImpl;
 
+import com.greatLearning.api.entity.User;
+import com.greatLearning.api.repository.UserRepository;
 import com.greatLearning.api.service.UserService.CreateUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateUserServiceImpl implements CreateUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void createUser(User user) {
+        this.userRepository.save(user);
+    }
 }
