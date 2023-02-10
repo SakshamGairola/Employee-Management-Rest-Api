613689a6199cb8f8900a58bb2b85ce29385f3bcc
Saksham <saksham.gairola28@gmail.com>
2023-02-10 20:22:03 +0530

    added method to fetch roles

--- src/main/java/com/greatLearning/api/service/RoleSevice/ReadRoleService.java
+++ src/main/java/com/greatLearning/api/service/RoleSevice/ReadRoleService.java
@@ -1,4 +1,9 @@
 package com.greatLearning.api.service.RoleSevice;
 
+import com.greatLearning.api.entity.Role;
+
+import java.util.List;
+
 public interface ReadRoleService {
+    List<Role> getALL();
 }
--- src/main/java/com/greatLearning/api/serviceImpl/RoleServiceImpl/ReadRoleServiceImpl.java
+++ src/main/java/com/greatLearning/api/serviceImpl/RoleServiceImpl/ReadRoleServiceImpl.java
@@ -1,8 +1,20 @@
 package com.greatLearning.api.serviceImpl.RoleServiceImpl;
 
+import com.greatLearning.api.entity.Role;
+import com.greatLearning.api.repository.RoleRepository;
 import com.greatLearning.api.service.RoleSevice.ReadRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReadRoleServiceImpl implements ReadRoleService {
+    @Autowired
+    RoleRepository repository;
+
+    @Override
+    public List<Role> getALL(){
+        return this.repository.findAll();
+    }
 }
