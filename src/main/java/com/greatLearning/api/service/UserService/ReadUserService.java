0bb0cacdac44a0cbf0d35126da8ea67f5277951c
Saksham <saksham.gairola28@gmail.com>
2023-02-11 16:47:35 +0530

    added method to fetch user

--- src/main/java/com/greatLearning/api/service/UserService/ReadUserService.java
+++ src/main/java/com/greatLearning/api/service/UserService/ReadUserService.java
@@ -1,5 +1,10 @@
 package com.greatLearning.api.service.UserService;
 
+import com.greatLearning.api.entity.User;
+
+import java.util.List;
+
 public interface ReadUserService {
+    List<User> getALL();
 }
 
--- src/main/java/com/greatLearning/api/serviceImpl/UserServiceImpl/ReadUserServiceImpl.java
+++ src/main/java/com/greatLearning/api/serviceImpl/UserServiceImpl/ReadUserServiceImpl.java
@@ -1,8 +1,20 @@
 package com.greatLearning.api.serviceImpl.UserServiceImpl;
 
+import com.greatLearning.api.entity.User;
+import com.greatLearning.api.repository.UserRepository;
 import com.greatLearning.api.service.UserService.ReadUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReadUserServiceImpl implements ReadUserService {
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public List<User> getALL() {
+        return this.repository.findAll();
+    }
 }
